c1c36a96eebe95f05f5f3aa87cf39bc265ed1ba2
PacificCoastBits <jimxnw@protonmail.com>
2022-07-05 23:14:49 -0700

    Add check for master edition.. we have standards

--- programs/nft-garbage-collector/src/lib.rs
+++ programs/nft-garbage-collector/src/lib.rs
@@ -1,5 +1,6 @@
 use anchor_lang::prelude::*;
-use anchor_spl::token::{self, Token, TokenAccount,Transfer, Mint};
+use anchor_spl::token::{self, Token, TokenAccount, Transfer, Mint};
+use metaplex_token_metadata::state::{PREFIX, EDITION, Metadata};
 
 declare_id!("F3KPspDVmA17HrYfE7nTVpk21egB6c1qxv287upKAJU1");
 
@@ -9,7 +10,7 @@ pub mod nft_garbage_collector {
 
     pub fn initialize_garbage_collection(ctx: Context<InitializeGarbageCollection>, should_mint_ticket: bool) -> Result<()> {
         //TODO
-        //-- 0. Confirm Token is NFT (This could probaby get moved to a helper func)
+        //----- 0. Confirm Token is NFT (This could probaby get moved to a helper func)
         let nft_account = &ctx.accounts.initializer_nft_account;
         let user = &ctx.accounts.initializer;
         let nft_mint_account = &ctx.accounts.initializer_nft_mint_account;
@@ -23,7 +24,26 @@ pub mod nft_garbage_collector {
         //Assert the amount on the Token account
         assert_eq!(nft_account.amount,1);
        
-        //Verify is Master edition NFT (To Avoid getting spammed with shit..)
+        
+        //Verify is Master edition NFT - To Avoid getting spammed with shit..
+        let master_edition_seed = &[
+            PREFIX.as_bytes(),
+            ctx.accounts.token_metadata_program.key.as_ref(),
+            nft_account.mint.as_ref(),
+            EDITION.as_bytes()
+        ];
+        
+        //Find the program adress and bump seeds
+        let (master_edition_key, master_edition_seed) = 
+            Pubkey::find_program_address(master_edition_seed, ctx.accounts.token_metadata_program.key);
+
+        // Assert that the nft metadata master edition passed in a valid master edition 
+        assert_eq!(master_edition_key, ctx.accounts.nft_metadata_account.key());
+
+        // Assert the master edition account is initalized
+        if ctx.accounts.nft_metadata_account.data_is_empty(){   
+            return Err(ErrorCode::AccountNotInitialized.into())
+        }
 
         //1. Transfer the NFT to the Garbage Wallet, with optional msg?
         //2. If should_mint_ticket Mint an museum Ticket/Recipt for the user
@@ -40,6 +60,7 @@ pub mod nft_garbage_collector {
  * I can do a "are you sure" in the client to save resources here. 
 */
 #[derive(Accounts)]
+#[instruction(metadata_program_id: Pubkey)]
 pub struct InitializeGarbageCollection<'info> {
     #[account(mut)]
     // The user
@@ -49,7 +70,9 @@ pub struct InitializeGarbageCollection<'info> {
     // The users nft account
     pub initializer_nft_account: Account<'info, TokenAccount>,
     // The account that holds the nft metadata
-    pub nft_metadata_account: Account<'info, TokenAccount>,
+    pub nft_metadata_account: AccountInfo<'info>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
+    #[account(address = metadata_program_id)]
+    pub token_metadata_program: AccountInfo<'info>
 }
